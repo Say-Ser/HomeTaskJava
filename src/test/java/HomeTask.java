5ab136e131d728de3794d80098a6fec8f7d6b3a5
Say-Ser <Sayapin94@yandex.ru>
2019-03-27 22:45:38 +0300

    Решение задач 0 и 1

---
+++ src/test/java/HomeTask.java
@@ -0,0 +1,26 @@
+public class HomeTask {
+    public static void main(String[] args) {
+    Task0(5,2);
+    System.out.println(Task1(2,4,6));
+
+    }
+    public static void Task0(int x, int y) {
+        int sum=x+y;
+        if(sum>=0)
+            System.out.println("Сумма "+sum+" положительна");
+        else
+            System.out.println("Сумма "+sum+" отрицательна");
+    }
+    public static int Task1(int x, int y, int z) {
+        if(x*y+37>0){
+            z=z*2;
+            return 7*x+y;
+        }
+        else {
+            if(x==0)
+                z=z/5;
+            else z=z/x;
+            return 5*y-x;
+        }
+    }
+}
