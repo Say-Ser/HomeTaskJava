b5731c8375f432ac9e6c3e6a3cec8ce7867b0e37
Say-Ser <Sayapin94@yandex.ru>
2019-03-27 22:51:15 +0300

    Добавление задачи 2

--- src/test/java/HomeTask.java
+++ src/test/java/HomeTask.java
@@ -2,7 +2,7 @@ public class HomeTask {
     public static void main(String[] args) {
     Task0(5,2);
     System.out.println(Task1(2,4,6));
-
+    Task2(10,12);
     }
     public static void Task0(int x, int y) {
         int sum=x+y;
@@ -23,4 +23,13 @@ public class HomeTask {
             return 5*y-x;
         }
     }
+    public static void Task2(int x, int y) {
+        int AVG=(x+y)/2;
+        int Xavg = (x-AVG)> 0 ? x-AVG : AVG-x ;
+        int Yavg = (y-AVG)> 0 ? y-AVG : AVG-y ;
+        System.out.println("Cредний возраст : "+AVG);
+        System.out.println("Отличие Тани от среденего "+Xavg);
+        System.out.println("Отличие Мити от среденего "+Yavg);
+    }
+
 }
