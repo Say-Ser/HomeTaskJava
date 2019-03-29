8161d751ef7ec7587a56f06fce1cbe32d50e7658
Say-Ser <Sayapin94@yandex.ru>
2019-03-29 22:57:31 +0300

    Добавление решений некоторых задач

--- src/test/java/HomeTask.java
+++ src/test/java/HomeTask.java
@@ -40,11 +40,43 @@ public class HomeTask {
         System.out.println("Температура по шкале Фаренгейта: "+Far);
         System.out.println("Температура по шкале Кельвина: "+Kel);
     }
+    public static int Task4(int x){//564
+        return x/10+x%10*100;
+    }
     public static void Task6(int m, int n){
         if(m%n==0)
             System.out.println(m/n);
         else System.out.println("Число "+m+" не делится нацело на число "+n+" !");
     }
+    public static void Task7(int x){
+
+        if(x/100>x%10)
+            System.out.println(x/100);
+        else  System.out.println(x%10);
+        if(x/100>x%100/10)
+            System.out.println(x/100);
+        else  System.out.println(x%100/10);
+        if(x%100/10>x%10)
+            System.out.println (x%100/10);
+        else  System.out.println (x%10);
+    }
+    public static void Task8(int x, int y){
+        boolean flag=false;
+        while (x/10>0){
+            if(x%10==y){
+               flag=true;
+               break;
+            }
+        }
+        if (flag)
+            System.out.println(y+" входит в число "+x);
+        else  System.out.println(y+" не входит в число "+x);
+    }
+    public static void Task9(int x){
+        String []w = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+        System.out.println(w[x-1]);
+    }
+
     public static int Task10(int N){
         int count=0;
         if(N<2){System.out.println("Четных чисел нет!");return 0;}
@@ -53,4 +85,22 @@ public class HomeTask {
         }
         return count;
     }
+
+    public static void Task11(int x, int y){
+        while (x%y!=0) {
+        x++;
+        }
+        System.out.println(x);
+    }
+    public static int Task12(String fk){
+        int count=0;
+        while (fk!='/0'){
+            fk=fk/10;
+            count++;
+        }
+        return count;
+    }
+
+    }
+
 }
