4384ca45db7a1e171b07bf4e79ee90382c46fed5
Say-Ser <Sayapin94@yandex.ru>
2019-04-01 19:58:27 +0300

    Вызов Решения всех задач (без 5 и 14)

--- src/test/java/HomeTask.java
+++ src/test/java/HomeTask.java
@@ -1,10 +1,21 @@
+
 public class HomeTask {
     public static void main(String[] args) {
     Task0(5,2);
     System.out.println(Task1(2,4,6));
     Task2(10,15);
     Task3(25);
+    System.out.println(Task4(564));
+    Task6(20,5);
+    Task7(654);
+    Task8(967,6);
+    Task9(7);
     System.out.println(Task10(25));
+    Task11(200,17);
+    System.out.println(Task12("Динамо"));
+    Task13("информатика");
+    Task15("Дети очень любят ели.");
+
     }
     public static void Task0(int x, int y) {
         int sum=x+y;
@@ -44,8 +55,6 @@ public class HomeTask {
 
         return x/10+x%10*100;
     }
-
-
     public static void Task6(int m, int n){
         if(m%n==0)
             System.out.println(m/n);
@@ -81,7 +90,6 @@ public class HomeTask {
         String []w = {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
         System.out.println(w[x-1]);
     }
-
     public static int Task10(int N){
         int count=0;
         if(N<2){System.out.println("Четных чисел нет!");return 0;}
@@ -90,7 +98,6 @@ public class HomeTask {
         }
         return count;
     }
-
     public static void Task11(int x, int y){
         while (x%y!=0) {
         x++;
@@ -98,9 +105,30 @@ public class HomeTask {
         System.out.println(x);
     }
     public static int Task12(String fk){
-        return fk.length();k
+        return fk.length();
     }
+    public  static  void Task13(String a){
+        String x=a.substring(2,7);
+        System.out.println(x);
+        String y=a.substring(7,10);
+        System.out.println(y);
+    }
+    public  static void Task15(String X){
+        char[] Y= X.toCharArray();
+        //а)
+        for(int i = 0; i<Y.length; i++){
+            if(Y[i] == 'е'){
+                System.out.println(i+1);
+                break;
+            }
+        }
+        //б)
+        for(int i = Y.length-1; i>0; i--){
+           if(Y[i] == 'е'){
+                System.out.println(i+1);
+                break;
+            }
+        }
 
     }
-
 }
